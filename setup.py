788e11f999f96342932ccbddcb12416893cab058
anna <a@mail.ru>
2022-06-13 10:29:50 +0300

    python version specified

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(include=['RulesTools', 'RulesTools.database_processing', 'RulesTools.filters',
                                     'RulesTools.transformations']),
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=['CGRtools', 'tqdm', 'StructureFingerprint', 'numpy', 'py-mini-racer>=0.4.0'],
     license='MIT'
 )
